4daf7118b1bd68826076effcf367563c6aae8047
Elchin Huseynli <elchin.kv@gmail.com>
2023-05-22 11:44:32 +0200

    navbar scroll animation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-import { greetUser } from '$utils/greet';
+let prevScrollpos = window.pageYOffset;
+const navbar = document.querySelector('.navigation');
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
-});
+// Create an IntersectionObserver instance
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      const currentScrollPos = window.pageYOffset;
+      if (!entry.isIntersecting) {
+        // If we're scrolling down, hide the navbar
+        if (prevScrollpos < currentScrollPos) {
+          navbar.style.transform = 'translateY(-120%)';
+        }
+      } else {
+        // If we're scrolling up, show the navbar
+        if (prevScrollpos > currentScrollPos) {
+          navbar.style.transform = 'translateY(0)';
+        }
+      }
+      prevScrollpos = currentScrollPos;
+    });
+  },
+  { threshold: [0.1] }
+); // 10% of the element is visible
+
+// Start observing an element
+const target = document.querySelector('#hero');
+observer.observe(target);
